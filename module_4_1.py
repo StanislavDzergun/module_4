750740251b565bef9e9e57efc791c87c4743110a
Stanislav <lyuba.dzergun@gmail.com>
2024-11-01 02:34:51 +0400

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+solution1 = fake_divide(69, 3)
+solution2 = fake_divide(3, 0)
+solution3 = true_divide(49, 7)
+solution4 = true_divide(15, 0)
+print(solution1)
+print(solution2)
+print(solution3)
+print(solution4)
